a61e2e649310ba12bd3639a18293ca8e66a96665
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-09-04 10:57:22 -0500

    test layout and req origin

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 var express = require('express')
 var ws = require('ws')
 var PORT = process.env.PORT || 3000
-var path = '' || `c:/users/ryanm/git/wst/`
 var server = express()
-
   .use((req, res) => res.sendFile(__dirname+'/index.html') )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 const wss = new ws.Server({ server });
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
+wss.on('connection', (ws, req) => {
+    console.log('Client connected', req.headers.origin);
     ws.on('close', () => console.log('Client disconnected'));
+    ws.on('message', (message)=>{console.log(ws); console.log(message)})
   });
 
 
