7de4ee453f6f8426f18b33cc563a7f99234ee05a
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-09-04 12:09:10 -0500

    server events complete

--- server.js
+++ server.js
@@ -7,22 +7,55 @@ var server = express()
 
 
 const wss = new ws.Server({ server });
-const broadcast = (data) =>{ wss.clients.forEach((client)=> {
 
+//broadcast server status to all connected clients
+const broadcast = () =>{ wss.clients.forEach( (client)=> {
+
+  var userlisthtml = '';
+  for (let ip in users)
+    if (users[ip].connected) userlisthtml+=(`<li class='userli'>${user[ip].name}</li>`);
+
+  var loghtml = log.map(entry=>`<span class='username'>${users[entry.ip].name}: </span>` + entry.event?
+                    `<span class='event'>${entry.event}</span>`
+                    :
+                    `<span class='message'>${entry.message}</span>`
+  ).join()
+  
+  client.send( {'users':userlisthtml, 'log':loghtml} );
 })}
 
+
+//initialize global server vars
 const users = {};
 const log = [];
 
+
+//set up connection event
 wss.on('connection', (ws, req) => {
+    //initialize connection
     let name = 'Guest';
     let ip = req.headers['x-forwarded-for'] || ws._socket.remoteAddress;
 
+    if (users[ip]) users[ip].connected=true;
+    else users[ip]={ 'name':name, 'connected':true }
+
     console.log('Client connected', ip);
+    log.push({'ip':ip, 'event':'connected'})
+    broadcast();
 
-    ws.on('close', ()=> {console.log('Client disconnected', ip)});
+    //set up connection events
+    ws.on('close', ()=> {
+      console.log('Client disconnected', ip);
+      users[ip].connected=false;
+      log.push({'ip':ip, 'event':'disconnected'})
+      broadcast();
+    });
 
-    ws.on('message', (message)=> {console.log(message)})
+    ws.on('message', (data)=> {
+      if (data.name!='') users[ip].name=data.name;
+      log.push({'ip':ip, 'message':data.message})
+      broadcast();
+    })
   });
 
 
