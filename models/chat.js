a9d22f6b290a37821bf7c90dc073c39d8d9d2634
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-09-24 21:48:58 -0500

    dont force chat model update

--- models/chat.js
+++ models/chat.js
@@ -1,4 +1,4 @@
-module.exports=(sequelize, Sequelize, force=true)=>{
+module.exports=(sequelize, Sequelize, force=false)=>{
   const Chat = sequelize.define('chat', {
       ip: {
         type: Sequelize.STRING, 
